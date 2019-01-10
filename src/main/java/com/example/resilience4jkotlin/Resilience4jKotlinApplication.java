5a16d277dfa167d7f644ac5a9bb0d957449e2bef
Napat Nateprachar <napat.nat@ascendcorp.com>
2019-01-10 23:52:09 +0700

    initialize project

---
+++ src/main/java/com/example/resilience4jkotlin/Resilience4jKotlinApplication.java
@@ -0,0 +1,14 @@
+package com.example.resilience4jkotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jKotlinApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/resilience4jkotlin/Resilience4jKotlinApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.resilience4jkotlin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Resilience4jKotlinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
