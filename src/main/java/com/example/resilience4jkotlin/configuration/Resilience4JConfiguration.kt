69248de9903119095e8cbab11b44829e9772eaa6
Napat Nateprachar <napat.nat@ascendcorp.com>
2019-01-22 21:53:57 +0700

    remove rate limiter

--- src/main/java/com/example/resilience4jkotlin/configuration/Resilience4JConfiguration.kt
+++ src/main/java/com/example/resilience4jkotlin/configuration/Resilience4JConfiguration.kt
@@ -4,10 +4,6 @@ import io.github.resilience4j.circuitbreaker.CircuitBreaker
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
 import io.github.resilience4j.circuitbreaker.autoconfigure.CircuitBreakerProperties
 import io.github.resilience4j.prometheus.CircuitBreakerExports
-import io.github.resilience4j.prometheus.RateLimiterExports
-import io.github.resilience4j.ratelimiter.RateLimiter
-import io.github.resilience4j.ratelimiter.RateLimiterRegistry
-import io.github.resilience4j.ratelimiter.autoconfigure.RateLimiterProperties
 import io.prometheus.client.CollectorRegistry
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
@@ -17,12 +13,10 @@ import org.springframework.context.annotation.Configuration
 class Resilience4JConfiguration {
 
     @Bean
-    fun registry(circuitBreakers: List<CircuitBreaker>, circuitBreakerRegistry: CircuitBreakerRegistry,
-                 rateLimiters: List<RateLimiter>, rateLimiterRegistry: RateLimiterRegistry)
+    fun registry(circuitBreakers: List<CircuitBreaker>, circuitBreakerRegistry: CircuitBreakerRegistry)
             : CollectorRegistry {
         val collectorRegistry = CollectorRegistry.defaultRegistry
         collectorRegistry.register(CircuitBreakerExports.ofCircuitBreakerRegistry(circuitBreakerRegistry))
-        collectorRegistry.register(RateLimiterExports.ofRateLimiterRegistry(rateLimiterRegistry))
         return collectorRegistry
     }
 
@@ -32,13 +26,5 @@ class Resilience4JConfiguration {
                                circuitBreakerProperties: CircuitBreakerProperties): CircuitBreaker {
         val config = circuitBreakerProperties.createCircuitBreakerConfig("nambawan")
         return circuitBreakerRegistry.circuitBreaker("nambawan", config);
-    }
-
-    @Bean
-    @Qualifier("nambawanRateLimiter")
-    fun nambawanRateLimiter(rateLimiterRegistry: RateLimiterRegistry,
-                            rateLimiterProperties: RateLimiterProperties): RateLimiter {
-        val config = rateLimiterProperties.createRateLimiterConfig("nambawan")
-        return rateLimiterRegistry.rateLimiter("nambawan", config);
     }
 }
